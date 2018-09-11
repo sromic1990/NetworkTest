38033d73e127bae1c756d15c066b50be2da62f91
Sourav Chatterjee <sromic@gmail.com>
2018-09-11 21:44:07 +0530

    - IP Address as per https://www.codeproject.com/Articles/10649/An-Introduction-to-Socket-Programming-in-NET-using

---
+++ Assets/Scripts/DisplayYourIP.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using Sourav.Utilities.Scripts.Attributes;
+
+public class DisplayYourIP : MonoBehaviour
+{
+    string hostname = Dns.GetHostName();
+
+    // Use this for initialization
+    void Start ()
+    {
+        DisplayIP();
+    }
+
+    [Button()]
+    private void DisplayIP()
+    {
+        Debug.Log("Hostname = " + hostname);
+        try
+        {
+            IPAddress[] addrs = Dns.GetHostEntry(hostname).AddressList;
+            foreach (IPAddress addr in addrs)
+            {
+                string str = string.Format("{0}/{1}", hostname, addr);
+                Debug.Log(str);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+}
---
+++ Assets/Scripts/RectTransformCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class RectTransformCheck : MonoBehaviour
+{
+    private RectTransform buttonRect;
+    private Vector3 localPos;
+    private Vector2 sizeDelta;
+    private bool toggle = false;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        buttonRect = GetComponent<RectTransform>();
+        localPos = buttonRect.localPosition;
+        sizeDelta = buttonRect.sizeDelta;
+
+        InvokeRepeating("DiminishRect", 1.0f, 2.0f);
+	}
+
+    void DiminishRect()
+    {
+        if(toggle)
+        {
+            buttonRect.localPosition = new Vector3(0, 0, 0);
+            buttonRect.sizeDelta = new Vector2(0, 0);
+        }
+        else
+        {
+            buttonRect.localPosition = localPos;
+            buttonRect.sizeDelta = sizeDelta;
+        }
+        toggle = !toggle;
+    }
+}
